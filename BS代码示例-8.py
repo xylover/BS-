775609f11e9a0965d852e2171fd7438c03bc9500
xylover <63713441+xylover@users.noreply.github.com>
2020-06-23 13:49:06 +0800

    BS代码示例

---
+++ BS代码示例-8.py
@@ -0,0 +1,18 @@
+html = """
+<html><head><title>The Dormouse's story</title></head>
+<body>
+<p class="story">
+    Once upon a time there were three little sisters; and their names were
+<a href="http://example.com/elsie" class="sister" id="link1">Elsie</a>,
+<a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
+<a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
+and they lived at the bottom of a well.</p>
+<p class="story">...</p>
+"""
+
+from bs4 import BeautifulSoup
+soup = BeautifulSoup(html , 'lxml')
+print('Next Sibling',soup.a.next_sibling)
+print('Prev Sibling',soup.a.previous_sibling)
+print('Next Siblings',list(enumerate(soup.a.next_siblings)))
+print('Prev Siblings',list(enumerate(soup.a.previous_siblings)))
